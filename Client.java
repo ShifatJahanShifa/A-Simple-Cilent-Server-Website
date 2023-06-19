213debaae83936a3d3fb02e20c2f8cb8a1a39adf
ShifatJahanShifa <bsse1301@iit.du.ac.bd>
2023-06-19 19:41:27 +0600

    it worked fine in terminal!

--- Client.java
+++ Client.java
@@ -1,2 +1,28 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+    public static void main(String[] args) throws IOException {
+        // creating client side socket = local host ip + port number
+        Socket socket=new Socket("127.0.0.1",1301);
+
+        // taking input from client
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter two numbers: ");
+        int number1,number2;
+        number1= input.nextInt();
+        number2= input.nextInt();
+
+        // giving information to server
+        PrintStream toServer=new PrintStream(socket.getOutputStream());
+        toServer.println(number1);
+        toServer.println(number2);
+
+        // receiving information from server
+        Scanner receiver=new Scanner(socket.getInputStream());
+        int result= receiver.nextInt();
+        System.out.println("sum of the two numbers is: "+result);
+    }
 }
--- Server.java
+++ Server.java
@@ -1,2 +1,26 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Server {
+    public static void main(String[] args) throws IOException {
+        // server side socket
+        ServerSocket serverSocket=new ServerSocket(1301);
+
+        // receiving or accepting from client
+        Socket clientsInfo=serverSocket.accept();  // establishing connection with clients
+        Scanner info=new Scanner(clientsInfo.getInputStream());
+
+        // process with the info
+        int number1,number2,sum;
+        number1= info.nextInt();
+        number2= info.nextInt();
+        sum=number1+number2;
+
+        // giving back result to client
+        PrintStream result=new PrintStream(clientsInfo.getOutputStream());
+        result.println(sum);
+    }
 }
